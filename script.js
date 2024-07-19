e4f8460d36fe075e755617e5d485e9db1cb3a3ac
Bibek244 <53641934+Bibek244@users.noreply.github.com>
2024-07-19 10:20:33 +0545

    added validation

---
+++ script.js
@@ -0,0 +1,105 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let submit = document.getElementById("form-submit");
+
+  submit.addEventListener("click", function () {
+    
+
+    event.preventDefault();
+    isValid = validateEmail() && validateMessage && validateName; 
+    if(isValid) {
+    form.submit();
+      alert("form successfully submitted");
+      console.log("form submitted");
+    }
+  });
+
+});
+function validateEmail(){
+    let email = document.getElementById('contact-email');
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let errEmail = document.getElementById('err-email');
+    
+    let isValid = true;
+
+    if (!email.value.trim()) {
+        errName.innerText = "Please fill in the name field";
+        email.focus();
+        isValid = false;
+    } else if (!emailPattern.test(email.value)) {
+        errEmail.innerText = "Only alphabets, spaces, and tab characters are valid for the name field";
+        email.focus();
+        isValid = false;
+    } else {
+        errEmail.innerText = '';
+        isValid = true;
+    }
+
+    return isValid;
+   
+}
+function validateName(){
+    let nameField = document.getElementById('name');
+    let namePattern = /^[A-Za-z\s\t]+$/;
+    let errName = document.getElementById('err-name');
+    let isValid = true;
+  
+    if (!nameField.value.trim()) {
+        errName.innerText = "Please fill in the name field";
+        nameField.focus();
+        isValid = false;
+    } else if (!namePattern.test(nameField.value)) {
+        errName.innerText = "Only alphabets, spaces, and tab characters are valid for the name field";
+        nameField.focus();
+        isValid = false;
+    } else {
+        errName.innerText = '';
+        isValid = true;
+    }
+  
+    return isValid;
+   
+  }
+  function validateEmail(){
+    let email = document.getElementById('contact-email');
+  
+    let errEmail = document.getElementById('err-email');
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    let isValid = true;
+  
+    if (!email.value.trim()) {
+        errName.innerText = "Please fill in the email field";
+        email.focus();
+        isValid = false;
+    } else if (!emailPattern.test(email.value)) {
+        errEmail.innerText = "Please enter correct email";
+        email.focus();
+        isValid = false;
+    } else {
+        errEmail.innerText = '';
+        isValid = true;
+    }
+  
+    return isValid;
+   
+  }
+
+function validateMessage(){
+    let message = document.getElementById("message");   
+    let errMessage = document.getElementById('err-message');
+    
+    let isValid = true;
+
+    if (!message.value || message.value.length == 0 ) {
+        errMessage.innerText = "Please fill in the message";
+        message.focus();
+        isValid = false;
+    } else {
+        errMessage.innerText = '';
+        isValid = true;
+    }
+
+    return isValid;
+   
+}
+
